14fe4655c02947f9da3dc0c93d918fa34c797428
TorHugo <arruda.victorhugo@outlook.com>
2023-08-22 01:44:49 -0300

    JetPack COMPOSE: - Espaçamento entre componentes (Spacer).

--- app/src/main/java/com/example/fiap_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/fiap_jetpack_compose/MainActivity.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fiap_jetpack_compose.ui.theme.MyAgeTheme
 
@@ -43,31 +51,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComponentMain() {
-    Column() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
         Text(
             text = "Qual a sua idade?",
             color = Color.Gray,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
+            textAlign = TextAlign.Center
         )
+        Spacer(modifier = Modifier.height(25.dp))
         Text(
             text = "Adicione a sua idade.",
             fontSize = 12.sp,
             color = Color.Blue,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
+            textAlign = TextAlign.Center
         )
-        Row (modifier = Modifier.fillMaxWidth()){
+        Spacer(modifier = Modifier.height(75.dp))
+        Row (modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
             Button(
                 onClick = {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD617C3), contentColor = Color(0xFF000000))
+                modifier = Modifier.size(64.dp),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD617C3),
+                                                        contentColor = Color(0xFF000000))
             ) {
                 Text(text = "-", fontSize = 24.sp)
             }
             Button(onClick = {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD617C3), contentColor = Color(0xFF000000))
+                modifier = Modifier.size(64.dp),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD617C3),
+                                                        contentColor = Color(0xFF000000))
             ) {
                 Text(text = "+", fontSize = 24.sp)
             }
