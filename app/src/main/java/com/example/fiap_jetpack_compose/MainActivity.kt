5f7fba3047281cf3424d28b134ec3b8b0f7d1fd5
TorHugo <arruda.victorhugo@outlook.com>
2023-08-24 01:51:28 -0300

    COMPONENTES: - Text(); - Video 02.

--- app/src/main/java/com/example/fiap_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/fiap_jetpack_compose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.fiap_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fiap_jetpack_compose.ui.theme.FiapjetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +43,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .background(Color.Black)
+    ) {
+        Text(
+            text = "FIAP",
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            fontSize = 32.sp,
+            color = Color(0xFFED145B),
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace
+        )
+        Text(
+            text = "Analise e Desenvolvimento de Sistemas.",
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            fontSize = 16.sp,
+            color = Color.White
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     FiapjetpackcomposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
